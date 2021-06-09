0877e8a41802762bb817546902f0b7bd60ec8908
Amine <algusamine@gmail.com>
2021-06-09 00:56:14 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+let names = ['Ada', 'Brendan', 'Ali'];
+let event = ['birthday'];
+function writeCards(names, event) {
+  let emptyArray = [];
+  for (let i = 0; i <= writeCards.length; i++) {
+      emptyArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return emptyArray;
+};
+writeCards(names, event);
+
+function countDown(){
+    let i = 10;
+    while (i >=0){
+      console.log(i);
+      i--;
+    };
+    }
+    countDown();
+  
